eb21728dfe0316e8021c779dcc243164117b2718
kjones <k@kristianjones.dev>
2022-05-28 23:35:03 +0000

    Update KubernetesWIP/Scripts/tinkSync.sh, KubernetesWIP/Scripts/genToken.sh

--- KubernetesWIP/Scripts/tinkSync.sh
+++ KubernetesWIP/Scripts/tinkSync.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
 ls -lah /manifests
+
+for FILE in /manifests/*-tinkhardware.yaml; do
+  echo "Processing hardware file ${FILE}"
+done
+
+for WorkflowFile in /manifests/*-tinkworkflow.yaml; do
+  echo "Processing worklow file ${WorkflowFile}"
+done
+
+echo "Done?"
