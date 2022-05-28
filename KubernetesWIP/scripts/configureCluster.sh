50f03c832dfcd45260016ac7d74381189649f9da
kjones <k@kristianjones.dev>
2022-05-28 19:05:11 +0000

    Update KubernetesWIP/scripts/configureCluster.sh

--- KubernetesWIP/scripts/configureCluster.sh
+++ KubernetesWIP/scripts/configureCluster.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
 set -e
 set -x
-
 ENDPOINT=$(awk -F'[ "]+' '$1 == "controlPlaneEndpoint:" {print $2}' /config/kubeadmcfg.yaml)
 
+# ------------------------------------------------------------------------------
+# Update secrets and component configs
+# ------------------------------------------------------------------------------
 
 # wait for cluster
-
 echo "Waiting for api-server endpoint ${ENDPOINT}..."
-
 until kubectl cluster-info >/dev/null 2>/dev/null; do
   sleep 1
 done
 
+
+# ------------------------------------------------------------------------------
+# Cluster configuration
+# ------------------------------------------------------------------------------
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 
 # upload configuration
-
 # TODO: https://github.com/kvaps/kubernetes-in-kubernetes/issues/6
-
 kubeadm init phase upload-config kubeadm --config /config/kubeadmcfg.yaml
+kubectl patch configmap -n kube-system kubeadm-config \
+  -p '{"data":{"ClusterStatus":"apiEndpoints: {}\napiVersion: kubeadm.k8s.io/v1beta2\nkind: ClusterStatus"}}'
 
 
 # upload configuration
-
 # TODO: https://github.com/kvaps/kubernetes-in-kubernetes/issues/5
-
 kubeadm init phase upload-config kubelet --config /config/kubeadmcfg.yaml -v1 2>&1 |
   while read line; do echo "$line" | grep 'Preserving the CRISocket information for the control-plane node' && killall kubeadm || echo "$line"; done
 
